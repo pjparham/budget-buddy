9ba818ec2f5b844a6fd88605b44e0a4cc87ec98c
Parker Parham <parkerparham@gmail.com>
2023-07-06 16:57:02 -0400

    Init client folder with react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Budget Buddy</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
