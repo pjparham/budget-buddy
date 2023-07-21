befa5263202b5be7e0a399895217f5d45cba3d33
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-07-21 01:06:35 -0400

    clean code

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,39 +8,12 @@ import {
     Input,
     Button,
     SimpleGrid,
-    Avatar,
-    AvatarGroup,
     useBreakpointValue,
-    IconProps,
     Icon,
   } from '@chakra-ui/react';
   import { FcMoneyTransfer, FcCalculator } from 'react-icons/fc';
-  
-//   const avatars = [
-//     {
-//       name: 'Ryan Florence',
-//       url: 'https://bit.ly/ryan-florence',
-//     },
-//     {
-//       name: 'Segun Adebayo',
-//       url: 'https://bit.ly/sage-adebayo',
-//     },
-//     {
-//       name: 'Kent Dodds',
-//       url: 'https://bit.ly/kent-c-dodds',
-//     },
-//     {
-//       name: 'Prosper Otemuyiwa',
-//       url: 'https://bit.ly/prosper-baba',
-//     },
-//     {
-//       name: 'Christian Nwamba',
-//       url: 'https://bit.ly/code-beast',
-//     },
-//   ];
-  
-  export default function JoinOurTeam() {
-    const avatarIMG = useBreakpointValue({ base: 'md', md: 'lg' });
+
+  export default function JoinBudgetBuddy() {
 
     return (
       <Box position={'relative'}>
