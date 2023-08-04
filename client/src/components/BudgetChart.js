cdd35cd2a17904b38820f39d08104bdd8c99de24
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-08-04 15:21:23 -0400

    added container to center piechart

---
+++ client/src/components/BudgetChart.js
@@ -0,0 +1,38 @@
+import { Container } from '@chakra-ui/react';
+import React from 'react'
+import { Chart } from 'react-google-charts'
+
+export default function BudgetChart({ budget, categories }) {
+    const data = [["Category", "Amount Allocated"]];
+
+    if(budget && categories){
+        data.push(["Remaining Amout", budget.remaining_amount])
+        categories.forEach((cat) => {
+            data.push([cat.title, cat.amount])
+        })
+    }
+      
+    const options = {
+        title: budget?.title || '',
+        backgroundColor: "transparent",
+        is3D: true,
+        titleTextStyle: {
+            color: '#B2BEB5'
+        },
+        legendTextStyle: {
+            color: '#B2BEB5'
+        },
+      }
+
+  return budget && categories ? (
+    <Container>
+        <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width="100%"
+        height="400px"
+        />
+    </Container>
+  ) : <><br/><h1>Loading...</h1></>;
+}
