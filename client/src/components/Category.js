2e3c66f71ed94dd029a58e389a03867c2fb2ae3f
Parker Parham <parkerparham@gmail.com>
2023-08-11 12:56:35 -0400

    Pulls loading screen into component for reusability, adds loading to budget page

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -5,6 +5,7 @@ import CategoryCard from './CategoryCard'
 import Navbar from './Navbar'
 import BudgetTable from './BudgetTable'
 import { Heading, Text, Container } from '@chakra-ui/react'
+import Loading from './Loading'
 
 const Category = () => {
     const { id } = useParams()
@@ -24,23 +25,7 @@ const Category = () => {
     return (
       <>
         <Navbar />
-        <Heading
-            as={motion.h1}
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -100 }}
-            fontWeight={600}
-            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
-            mb={'25'}
-            lineHeight={'110%'}>
-        <Text as={'span'} 
-            bgGradient="linear(to-r, green.400,green.700)"
-            bgClip="text"
-            >
-        <br />
-        Loading...
-        </Text>
-    </Heading>
+        <Loading/>
       </>
     )
   }
